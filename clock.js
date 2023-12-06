8cff4746eedd5d17fa0a7770de5ccd3451d255a5
Xilerth <xilerth@gmail.com>
2023-12-06 18:42:55 +0100

    min

--- clock.js
+++ clock.js
@@ -1,5 +1,1 @@
-//generate a js clock web component
-
-class Clock extends HTMLElement {
-  constructor() {super(), this.attachShadow({ mode: "open" });let e = document.createElement("template");e.innerHTML = `<style>:host {display: block;padding: 16px;background-color: lightblue;}</style><p>This is a simple clock web component!</p>`;this.shadowRoot.appendChild(e.content.cloneNode(!0));this.connectedCallback();}connectedCallback() {this.startClock();}startClock() {setInterval(() => {this.shadowRoot.querySelector("p").innerText = new Date().toLocaleTimeString();}, 1000);}
-}customElements.define("my-clock", Clock);
+class Clock extends HTMLElement {constructor() {super(), this.attachShadow({ mode: "open" });let e = document.createElement("template");e.innerHTML = `<style>:host {display: block;padding: 16px;background-color: lightblue;}</style><p>This is a simple clock web component!</p>`;this.shadowRoot.appendChild(e.content.cloneNode(!0));this.connectedCallback();}connectedCallback() {this.startClock();}startClock() {setInterval(() => {this.shadowRoot.querySelector("p").innerText = new Date().toLocaleTimeString();}, 1000);}}customElements.define("my-clock", Clock);
