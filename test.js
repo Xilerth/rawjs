9a2280e2497af1f40919c22c5da33d4442449287
Xilerth <xilerth@gmail.com>
2023-12-06 18:26:41 +0100

    main

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-class testCoponent extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"});let e=document.createElement("template");e.innerHTML=`<style>:host {display: block;padding: 16px;background-color: lightblue;}</style><p>This is a simple web component!</p>`,this.shadowRoot.appendChild(e.content.cloneNode(!0))}}customElements.define("my-component",testCoponent);
+class testCoponent extends HTMLElement {
+  constructor() {
+    super(), this.attachShadow({ mode: "open" });
+    let e = document.createElement("template");
+    (e.innerHTML = `<style>:host {display: block;padding: 16px;background-color: lightblue;}</style><p>This is a simple web component!</p>`),
+      this.shadowRoot.appendChild(e.content.cloneNode(!0));
+  }
+}
+customElements.define("my-component", testCoponent);
